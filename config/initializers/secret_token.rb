685362632f0aa3ee37f5e2cafac2a02464408a47
messiahxu <messiahxu@gmail.com>
2013-07-22 04:30:43 +0800

    Init and static_pages controller home began

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AskFm::Application.config.secret_key_base = 'fbe689b68e35105b933ba021f162269fa42d13c984fd3c839e57241a607adc55fd14af911797655c2f51f0a1775a8597a0fe065be55891868a46f6db01b540a9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AskFm::Application.config.session_store :cookie_store, key: '_ask_fm_session'
