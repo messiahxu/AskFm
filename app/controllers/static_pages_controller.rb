dd12501edd9f590058b830f7b41ad903d3db8f5d
messiahxu <messiahxu@gmail.com>
2013-08-03 10:50:56 +0800

    将登录之后的首页跳转到 Timeline

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,8 @@ class StaticPagesController < ApplicationController
   before_action :signed_in_user?, only: [:timeline]
   def home
     if signed_in?
-      redirect_to user_path(current_user)
-      #render :timeline
+      #redirect_to user_path(current_user)
+      render :timeline
     end
   end
 
