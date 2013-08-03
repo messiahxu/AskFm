29234816622f1d0d949841326cf320bb388825f7
messiahxu <messiahxu@gmail.com>
2013-08-03 11:18:04 +0800

    fix bug

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,11 +3,17 @@ class StaticPagesController < ApplicationController
   def home
     if signed_in?
       #redirect_to user_path(current_user)
+      feeds
       render :timeline
     end
   end
 
   def timeline
-    @feed_items = current_user.feed.page(params[:page]).per(20)
+    feeds
   end
+
+  private
+    def feeds
+      @feed_items = current_user.feed.page(params[:page]).per(20)
+    end
 end
